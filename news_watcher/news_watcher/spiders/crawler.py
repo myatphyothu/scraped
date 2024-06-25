91851a6d61183fcd4b2cd49012652c536e9ee7e7
myatphyothu <phyothu.myat@gmail.com>
2024-06-25 20:33:36 +0800

    news_watcher crawler updates for caeh.ca page

--- news_watcher/news_watcher/spiders/crawler.py
+++ news_watcher/news_watcher/spiders/crawler.py
@@ -1,4 +1,5 @@
 import scrapy
+from bs4 import BeautifulSoup
 
 
 class Crawler(scrapy.Spider):
@@ -6,6 +7,7 @@ class Crawler(scrapy.Spider):
     name = 'news_watcher'
     allowed_domains = ['caeh.ca']
     start_urls = ['https://caeh.ca/category/news']
+
     PROXY_SERVER = None
 
     def parse(self, response):
@@ -13,10 +15,15 @@ class Crawler(scrapy.Spider):
         for css in links_css:
             yield response.follow(css.get(), callback=self.parse_news)
 
+    def drilldown_content(self, response):
+        raw_content = ''.join(response.css('div.entry p').getall())
+        return BeautifulSoup(raw_content).get_text()
+
     def parse_news(self, response):
         title = response.css('p.postTitle::text').get().strip()
         url = response.request.url
-        content = response.css('div.entry p::text').getall()
+
+        content = self.drilldown_content(response)
 
         yield {
             'title': title,
